b72ce54358bd94ae4c5735462a7f27f709a2cc89
NishanDas <nishandas880@gmail.com>
2022-03-01 00:58:11 +0600

    search function working

---
+++ js/app.js
@@ -0,0 +1,7 @@
+ const loadSearchData =()=>{
+    const searchFiled=document.getElementById('input-filed');
+}
+
+
+
+// const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`
