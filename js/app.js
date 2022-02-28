df2dc2a9653fb12a7fe3edb21bfee6768d462755
NishanDas <nishandas880@gmail.com>
2022-03-01 02:20:33 +0600

    details area finshed

--- js/app.js
+++ js/app.js
@@ -29,5 +29,34 @@ const displayResult=(phones)=>{
         phoneContainer.appendChild(div);
     });
 }
+// loading phones details
+const loadPhoneDetails=(phoneID)=>{
+    const url=`https://openapi.programming-hero.com/api/phone/${phoneID}`;
+    fetch(url)
+    .then(res =>res.json())
+    .then(data => displayPhoneDetails(data.data))
+}
+const displayPhoneDetails =(phone)=>{
+    console.log(phone)
+    const detailsContainer=document.getElementById('details-container');
+    const div=document.createElement('div');
+    div.innerHTML=`
+        <div class="col">
+          <div class="card w-50 mx-auto p-3">
+            <img src="${phone.image}" class="h-50 w-50 mx-auto " alt="..." />
+            <div class="card-body">
+              <h4 class="card-title">Name:${phone.name}</h4>
+              <h4>Brand:${phone.brand}</h4>
+              <h5>${phone.releaseDate}</h5>
+              <p>Chip:${phone.mainFeatures.chipSet}.<br>display:${phone.mainFeatures.displaySize}.<br>memory:${phone.mainFeatures.memory}.</p>
+            </div>
+          </div>
+        </div>
+        `;
+        detailsContainer.appendChild(div);
 
-// const dispalySearchData=(phones)
+}
+
+
+
+
