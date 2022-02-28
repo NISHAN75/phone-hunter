bb501ac7a7dfae70bca8ea41283e380ab843ed46
NishanDas <nishandas880@gmail.com>
2022-03-01 01:40:08 +0600

    searching area clear

--- js/app.js
+++ js/app.js
@@ -11,19 +11,17 @@ const loadSearchData =()=>{
 // dispaly in searching Data
 const displayResult=(phones)=>{
     phones.forEach(phone => {
+        console.log(phone)
         const phoneContainer=document.getElementById('phone-container');
         const div=document.createElement('div');
         div.innerHTML=`
         <div class="col">
-          <div class="card">
-            <img src="..." class="card-img-top" alt="..." />
+          <div class="card p-3">
+            <img src="${phone.image}" class="h-50 w-50 mx-auto " alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+              <h5 class="card-title">Name:${phone.phone_name}</h5>
+              <h5>Brand:${phone.brand}</h5>
+              <button class="bg-info" onclick="loadPhoneDetails('${phone.slug}')">Details</button>
             </div>
           </div>
         </div>
