4382deb9b73e84fccd3389413bac7cd2822e71c2
NishanDas <nishandas880@gmail.com>
2022-03-01 01:21:14 +0600

    data seaching working ok

--- js/app.js
+++ js/app.js
@@ -1,7 +1,35 @@
- const loadSearchData =()=>{
+// load Searching data 
+const loadSearchData =()=>{
     const searchFiled=document.getElementById('input-filed');
+    const searchText=searchFiled.value;
+    // data calling
+    const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+    .then(res =>res.json())
+    .then(data => displayResult(data.data))
+}
+// dispaly in searching Data
+const displayResult=(phones)=>{
+    phones.forEach(phone => {
+        const phoneContainer=document.getElementById('phone-container');
+        const div=document.createElement('div');
+        div.innerHTML=`
+        <div class="col">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+        </div>
+        `;
+        phoneContainer.appendChild(div);
+    });
 }
 
-
-
-// const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`
+// const dispalySearchData=(phones)
